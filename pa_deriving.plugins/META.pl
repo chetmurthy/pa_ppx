c633067d6318277cb656d2ae9683d85559fd06c0
Chet Murthy <chetsky@gmail.com>
2020-04-21 20:37:57 -0700

    starting ppx_deriving_yojson

--- pa_deriving.plugins/META.pl
+++ pa_deriving.plugins/META.pl
@@ -13,14 +13,14 @@ description = "pa_ppx_pa_deriving_plugins support"
 directory = "$destdir/pa_ppx_pa_deriving_plugins"
 
 # For linking
-archive(byte) = "pa_deriving_show.cmo pa_deriving_eq.cmo pa_deriving_ord.cmo pa_deriving_enum.cmo pa_deriving_iter.cmo pa_deriving_map.cmo pa_deriving_fold.cmo pa_deriving_make.cmo"
-archive(native) = "pa_deriving_show.cmx pa_deriving_eq.cmx pa_deriving_ord.cmx pa_deriving_enum.cmx pa_deriving_iter.cmx pa_deriving_map.cmx pa_deriving_fold.cmx pa_deriving_make.cmx"
+archive(byte) = "pa_deriving_show.cmo pa_deriving_eq.cmo pa_deriving_ord.cmo pa_deriving_enum.cmo pa_deriving_iter.cmo pa_deriving_map.cmo pa_deriving_fold.cmo pa_deriving_make.cmo pa_deriving_yojson.cmo"
+archive(native) = "pa_deriving_show.cmx pa_deriving_eq.cmx pa_deriving_ord.cmx pa_deriving_enum.cmx pa_deriving_iter.cmx pa_deriving_map.cmx pa_deriving_fold.cmx pa_deriving_make.cmx pa_deriving_yojson.cmx"
 
 # For the toploop:
-archive(byte,toploop) = "pa_deriving_show.cmo pa_deriving_eq.cmo pa_deriving_ord.cmo pa_deriving_enum.cmo pa_deriving_iter.cmo pa_deriving_map.cmo pa_deriving_fold.cmo pa_deriving_make.cmo"
+archive(byte,toploop) = "pa_deriving_show.cmo pa_deriving_eq.cmo pa_deriving_ord.cmo pa_deriving_enum.cmo pa_deriving_iter.cmo pa_deriving_map.cmo pa_deriving_fold.cmo pa_deriving_make.cmo pa_deriving_yojson.cmo"
 
 # For the preprocessor itself:
-archive(syntax,preprocessor) = "pa_deriving_show.cmo pa_deriving_eq.cmo pa_deriving_ord.cmo pa_deriving_enum.cmo pa_deriving_iter.cmo pa_deriving_map.cmo pa_deriving_fold.cmo pa_deriving_make.cmo"
+archive(syntax,preprocessor) = "pa_deriving_show.cmo pa_deriving_eq.cmo pa_deriving_ord.cmo pa_deriving_enum.cmo pa_deriving_iter.cmo pa_deriving_map.cmo pa_deriving_fold.cmo pa_deriving_make.cmo pa_deriving_yojson.cmo"
 
 package "show" (
   requires(syntax,toploop) = "camlp5,pa_ppx_pa_deriving"
@@ -117,6 +117,19 @@ package "make" (
 
   requires(byte) = "camlp5,pa_ppx_pa_deriving"
   archive(byte) = "pa_deriving_make.cmo"
+)
+
+package "yojson" (
+  requires(syntax,toploop) = "camlp5,pa_ppx_pa_deriving"
+  archive(syntax,toploop) = "pa_deriving_yojson.cmo"
+
+  package "syntax" (
+    requires(syntax,preprocessor) = "camlp5,pa_ppx_pa_deriving"
+    archive(syntax,preprocessor) = "pa_deriving_yojson.cmo"
+  )
+
+  requires(byte) = "camlp5,pa_ppx_pa_deriving"
+  archive(byte) = "pa_deriving_yojson.cmo"
 )
 
 EOF
---
+++ pa_deriving.plugins/pa_deriving_yojson.ml
@@ -0,0 +1,259 @@
+(* camlp5r *)
+(* pa_deriving_yojson.ml,v *)
+(* Copyright (c) INRIA 2007-2017 *)
+
+#load "q_MLast.cmo";
+#load "pa_extfun.cmo";
+
+open Asttools;
+open MLast;
+open Pa_passthru ;
+open Ppxutil ;
+open Surveil ;
+
+module Ctxt = struct
+  include Pa_passthru.Ctxt ;
+
+end ;
+
+value to_yojson_fname arg tyname =
+  if tyname = "t" then "to_yojson"
+  else tyname^"_to_yojson"
+;
+
+value of_yojson_fname arg tyname =
+  if tyname = "t" then "of_yojson"
+  else tyname^"_of_yojson"
+;
+
+type attrmod_t = [ Nobuiltin ] ;
+
+value to_expression arg param_map ty0 =
+  let rec fmtrec ?{attrmod=None} = fun [
+
+  <:ctyp:< $lid:lid$ >> when attrmod = Some Nobuiltin ->
+  let fname = to_yojson_fname arg lid in
+  <:expr< $lid:fname$ >>
+
+| <:ctyp:< _ >> -> <:expr< let open Fmt in (const string "_") >>
+| <:ctyp:< unit >> -> <:expr< fun ofmt arg -> let open Fmt in (pf ofmt "()") >>
+| <:ctyp:< int >> -> <:expr< fun ofmt arg -> let open Fmt in (pf ofmt "%d" arg) >>
+| <:ctyp:< bool >> -> <:expr<  Fmt.bool >>
+| <:ctyp:< int32 >> -> <:expr< fun ofmt arg -> let open Fmt in (pf ofmt "%ldl" arg) >>
+| <:ctyp:< int64 >> -> <:expr< fun ofmt arg -> let open Fmt in (pf ofmt "%LdL" arg) >>
+| (<:ctyp:< string >> | <:ctyp:< Stdlib.String.t >> | <:ctyp:< String.t >>) ->
+  <:expr< fun ofmt arg -> let open Fmt in (pf ofmt "%S" arg) >>
+| <:ctyp:< bytes >> -> <:expr< fun ofmt arg -> let open Fmt in (pf ofmt "%S" (Bytes.to_string arg)) >>
+| <:ctyp:< char >> -> <:expr< fun ofmt arg -> let open Fmt in (pf ofmt "%C" arg) >>
+| <:ctyp:< nativeint >> -> <:expr< fun ofmt arg -> let open Fmt in (pf ofmt "%an" nativeint arg) >>
+| <:ctyp:< float >> -> <:expr< fun ofmt arg -> let open Fmt in (pf ofmt "%F" arg) >>
+
+| <:ctyp:< $t$ [@ $attrid:id$ ] >> when id = DC.allowed_attribute (DC.get arg) "yojson" "nobuiltin" ->
+    fmtrec ~{attrmod=Some Nobuiltin} t
+
+| <:ctyp:< $t$ [@ $attribute:_$ ] >> -> fmtrec ~{attrmod=attrmod} t
+
+| <:ctyp:< $t1$ $t2$ >> -> <:expr< $fmtrec t1$ $fmtrec t2$ >>
+
+| <:ctyp:< '$i$ >> ->
+  let fmtf = match List.assoc i param_map with [
+    x -> x | exception Not_found -> failwith "pa_deriving.yojson: unrecognized param-var in type-decl"
+  ] in
+  <:expr< $lid:fmtf$ >>
+
+| <:ctyp:< $lid:lid$ >> ->
+  let fname = to_yojson_fname arg lid in
+  <:expr< $lid:fname$ >>
+| <:ctyp:< $longid:li$ . $lid:lid$ >> ->
+  let fname = to_yojson_fname arg lid in
+  Expr.prepend_longident li <:expr< $lid:fname$ >>
+| [%unmatched_vala] -> failwith "pa_deriving_yojson.fmt_expression"
+]
+in fmtrec ty0
+;
+
+value of_expression arg param_map ty0 =
+  let rec fmtrec ?{attrmod=None} = fun [
+
+  <:ctyp:< $lid:lid$ >> when attrmod = Some Nobuiltin ->
+  let fname = of_yojson_fname arg lid in
+  <:expr< $lid:fname$ >>
+
+| <:ctyp:< _ >> -> <:expr< let open Fmt in (const string "_") >>
+| <:ctyp:< unit >> -> <:expr< fun ofmt arg -> let open Fmt in (pf ofmt "()") >>
+| <:ctyp:< int >> -> <:expr< fun ofmt arg -> let open Fmt in (pf ofmt "%d" arg) >>
+| <:ctyp:< bool >> -> <:expr<  Fmt.bool >>
+| <:ctyp:< int32 >> -> <:expr< fun ofmt arg -> let open Fmt in (pf ofmt "%ldl" arg) >>
+| <:ctyp:< int64 >> -> <:expr< fun ofmt arg -> let open Fmt in (pf ofmt "%LdL" arg) >>
+| (<:ctyp:< string >> | <:ctyp:< Stdlib.String.t >> | <:ctyp:< String.t >>) ->
+  <:expr< fun ofmt arg -> let open Fmt in (pf ofmt "%S" arg) >>
+| <:ctyp:< bytes >> -> <:expr< fun ofmt arg -> let open Fmt in (pf ofmt "%S" (Bytes.to_string arg)) >>
+| <:ctyp:< char >> -> <:expr< fun ofmt arg -> let open Fmt in (pf ofmt "%C" arg) >>
+| <:ctyp:< nativeint >> -> <:expr< fun ofmt arg -> let open Fmt in (pf ofmt "%an" nativeint arg) >>
+| <:ctyp:< float >> -> <:expr< fun ofmt arg -> let open Fmt in (pf ofmt "%F" arg) >>
+
+| <:ctyp:< $t$ [@ $attrid:id$ ] >> when id = DC.allowed_attribute (DC.get arg) "yojson" "nobuiltin" ->
+    fmtrec ~{attrmod=Some Nobuiltin} t
+
+| <:ctyp:< $t$ [@ $attribute:_$ ] >> -> fmtrec ~{attrmod=attrmod} t
+
+
+| <:ctyp:< $t1$ $t2$ >> -> <:expr< $fmtrec t1$ $fmtrec t2$ >>
+
+| <:ctyp:< '$i$ >> ->
+  let fmtf = match List.assoc i param_map with [
+    x -> x | exception Not_found -> failwith "pa_deriving.yojson: unrecognized param-var in type-decl"
+  ] in
+  <:expr< $lid:fmtf$ >>
+
+| <:ctyp:< $lid:lid$ >> ->
+  let fname = of_yojson_fname arg lid in
+  <:expr< $lid:fname$ >>
+| <:ctyp:< $longid:li$ . $lid:lid$ >> ->
+  let fname = of_yojson_fname arg lid in
+  Expr.prepend_longident li <:expr< $lid:fname$ >>
+
+| [%unmatched_vala] -> failwith "pa_deriving_yojson.fmt_expression"
+]
+in fmtrec ty0
+;
+
+value fmt_to_top arg params = fun [
+  <:ctyp< $t1$ == $_priv:_$ $t2$ >> ->
+  to_expression arg params t2
+| t -> to_expression arg params t
+]
+;
+
+value fmt_of_top arg params = fun [
+  <:ctyp< $t1$ == $_priv:_$ $t2$ >> ->
+  of_expression arg params t2
+| t -> of_expression arg params t
+]
+;
+
+value str_item_top_funs arg (loc, tyname) param_map ty =
+  let tyname = Pcaml.unvala tyname in
+
+  let (to_fname, to_exp) =
+    let to_yojsonfname = to_yojson_fname arg tyname in
+    let to_e = fmt_to_top arg param_map ty in
+    let paramfun_patts = List.map (fun (_,ppf) -> <:patt< $lid:ppf$ >>) param_map in
+    (to_yojsonfname, Expr.abstract_over paramfun_patts
+       <:expr< fun arg -> $to_e$ arg >>) in
+
+  let (of_fname, of_exp) =
+    let of_yojsonfname = of_yojson_fname arg tyname in
+    let of_e = fmt_of_top arg param_map ty in
+    let paramfun_patts = List.map (fun (_,ppf) -> <:patt< $lid:ppf$ >>) param_map in
+    (of_yojsonfname, Expr.abstract_over paramfun_patts
+       <:expr< fun arg -> $of_e$ arg >>) in
+  [(to_fname, to_exp); (of_fname, of_exp)]
+;
+
+value sig_item_top_funs arg (loc, tyname) param_map ty =
+  let tyname = Pcaml.unvala tyname in
+
+  let (to_fname, to_type) =
+    let to_yojsonfname = to_yojson_fname arg tyname in
+    let paramtys = List.map (fun (tyna, _) -> <:ctyp< '$tyna$ >>) param_map in
+    let argfmttys = List.map (fun pty -> <:ctyp< $pty$ -> Yojson.Safe.t >>) paramtys in  
+    let ty = <:ctyp< $lid:tyname$ >> in
+    let toftype = Ctyp.arrows_list loc argfmttys <:ctyp< $(Ctyp.applist ty paramtys)$ -> Yojson.Safe.t >> in
+    (to_yojsonfname, toftype) in
+
+  let (of_fname, of_type) =
+    let of_yojsonfname = of_yojson_fname arg tyname in
+    let paramtys = List.map (fun (tyna, _) -> <:ctyp< '$tyna$ >>) param_map in
+    let argfmttys = List.map (fun pty -> <:ctyp< Yojson.Safe.t -> Rresult.R.result $pty$ string >>) paramtys in  
+    let ty = <:ctyp< $lid:tyname$ >> in
+    let offtype = Ctyp.arrows_list loc argfmttys <:ctyp< Yojson.Safe.t -> Rresult.R.result $(Ctyp.applist ty paramtys)$ string >> in
+    (of_yojsonfname, offtype) in
+    [(of_fname, of_type); (to_fname, to_type)]
+;
+
+value str_item_funs arg ((loc,_) as tyname) params ty =
+  let param_map = List.mapi (fun i p ->
+    match Pcaml.unvala (fst p) with [
+      None -> failwith "cannot derive yojson-functions for type decl with unnamed type-vars"
+    | Some na -> (na, Printf.sprintf "tp_%d" i)
+    ]) params in
+  let l = str_item_top_funs arg tyname param_map ty in
+  let types = sig_item_top_funs arg tyname param_map ty in
+  List.map (fun (fname, body) ->
+      let fty = List.assoc fname types in
+      let fty = if param_map = [] then fty
+        else <:ctyp< ! $list:(List.map fst param_map)$ . $fty$ >> in
+      (<:patt< ( $lid:fname$ : $fty$ ) >>, body, <:vala< [] >>)) l
+;
+
+value sig_item_funs arg ((loc,_) as tyname) params ty =
+  let param_map = List.mapi (fun i p ->
+    match Pcaml.unvala (fst p) with [
+      None -> failwith "cannot derive yojson-functions for type decl with unnamed type-vars"
+    | Some na -> (na, Printf.sprintf "tp_%d" i)
+    ]) params in
+  let l = sig_item_top_funs arg tyname param_map ty in
+  List.map (fun (fname, ty) ->
+      <:sig_item< value $lid:fname$ : $ty$>>) l
+;
+
+value is_deriving_yojson attr = Pa_deriving.is_deriving "yojson" attr ;
+value apply_deriving_yojson ctxt attr = Pa_deriving.apply_deriving "yojson" ctxt attr ;
+
+value str_item_gen_yojson0 arg td =
+  let arg = List.fold_left apply_deriving_yojson arg (Pcaml.unvala td.tdAttributes) in
+  let tyname = Pcaml.unvala td.tdNam
+  and params = Pcaml.unvala td.tdPrm
+  and tk = td.tdDef in
+  str_item_funs arg tyname params tk
+;
+
+value loc_of_type_decl td = fst (Pcaml.unvala td.tdNam) ;
+
+value str_item_gen_yojson arg = fun [
+  <:str_item:< type $_flag:_$ $list:tdl$ >> ->
+    let loc = loc_of_type_decl (List.hd tdl) in
+    let l = List.concat (List.map (str_item_gen_yojson0 arg) tdl) in
+    <:str_item< value rec $list:l$ >>
+| _ -> assert False ]
+;
+
+value sig_item_gen_yojson0 arg td =
+  let tyname = Pcaml.unvala td.tdNam
+  and params = Pcaml.unvala td.tdPrm
+  and tk = td.tdDef in
+  sig_item_funs arg tyname params tk
+;
+
+value sig_item_gen_yojson arg = fun [
+  <:sig_item:< type $_flag:_$ $list:tdl$ >> ->
+    let loc = loc_of_type_decl (List.hd tdl) in
+    let l = List.concat (List.map (sig_item_gen_yojson0 arg) tdl) in
+    <:sig_item< declare $list:l$ end >>
+| _ -> assert False ]
+;
+
+value expr_yojson arg = fun [
+  <:expr:< [% $attrid:id$: $type:ty$ ] >> when id = "to_yojson" || id = "derive.to_yojson" ->
+    let loc = loc_of_ctyp ty in
+    let e = fmt_to_top arg [] ty in
+    <:expr< fun arg -> Format.asprintf "%a" $e$ arg >>
+| <:expr:< [% $attrid:id$: $type:ty$ ] >> when id = "of_yojson" || id = "derive.of_yojson" ->
+    let loc = loc_of_ctyp ty in
+    let e = fmt_of_top arg [] ty in
+    <:expr< fun arg -> Format.asprintf "%a" $e$ arg >>
+| _ -> assert False ]
+;
+
+Pa_deriving.(Registry.add PI.{
+  name = "yojson"
+; options = ["optional"]
+; default_options = let loc = Ploc.dummy in [ ("optional", <:expr< False >>) ]
+; alg_attributes = ["nobuiltin"; "key"; "name"; "encodin"; "default"]
+; expr = expr_yojson
+; str_item = str_item_gen_yojson
+; sig_item = sig_item_gen_yojson
+})
+;
+
