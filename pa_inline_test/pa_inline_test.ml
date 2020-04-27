420c8f23223939ef17d9b2963ba65b2565a4999a
Chet Murthy <chetsky@gmail.com>
2020-04-26 22:55:56 -0700

    test_unit

--- pa_inline_test/pa_inline_test.ml
+++ pa_inline_test/pa_inline_test.ml
@@ -31,19 +31,48 @@ value bool_test arg descr e =
   >>
 ;
 
+value unit_test arg descr e =
+  let loc = loc_of_expr e in
+  let startpos = start_position_of_loc loc in
+  let endpos = end_position_of_loc loc in
+  let filename = startpos.Lexing.pos_fname in
+  let line_number = string_of_int startpos.Lexing.pos_lnum in
+  let start_pos = string_of_int (startpos.Lexing.pos_cnum - startpos.Lexing.pos_bol) in
+  let end_pos = string_of_int (endpos.Lexing.pos_cnum - endpos.Lexing.pos_bol) in
+  <:str_item< 
+  Ppx_inline_test_lib.Runtime.test_unit ~{config=(module Inline_test_config)}
+    ~{descr = $str:descr$} ~{tags=[]} ~{filename = $str:filename$}
+    ~{line_number= $int:line_number$} ~{start_pos = $int:start_pos$} ~{end_pos = $int:end_pos$}
+     (fun () -> $exp:e$)
+  >>
+;
+
 value rewrite_str_item arg = fun [
   <:str_item:< [%%test $exp:e$ ; ] >>
   ->
   let descr = Printf.sprintf ": <<%s>>" (prettyprint_expr e) in
   bool_test arg descr e
 
+| <:str_item:< [%%test_unit $exp:e$ ; ] >>
+  ->
+  let descr = Printf.sprintf ": <<%s>>" (prettyprint_expr e) in
+  unit_test arg descr e
+
 | <:str_item:< [%%test value $flag:False$ $list:[(p,e,_)]$ ; ] >> ->
   let descr = match p with [
     <:patt< _ >> -> Printf.sprintf ": <<%s>>" (prettyprint_expr e)
-  | <:patt< $str:descr$ >> -> descr
+  | <:patt< $str:descr$ >> -> ": "^descr
   | _ -> failwith "pa_inline_test.rewrite_str_item: bad lhs of let"
   ] in
   bool_test arg descr e
+
+| <:str_item:< [%%test_unit value $flag:False$ $list:[(p,e,_)]$ ; ] >> ->
+  let descr = match p with [
+    <:patt< _ >> -> Printf.sprintf ": <<%s>>" (prettyprint_expr e)
+  | <:patt< $str:descr$ >> -> ": "^descr
+  | _ -> failwith "pa_inline_test.rewrite_str_item: bad lhs of let"
+  ] in
+  unit_test arg descr e
 ]
 ;
 
@@ -67,6 +96,12 @@ value is_named_bool_test = fun [
 ]
 ;
 
+value is_named_unit_test = fun [
+  <:str_item:< [%%test_unit value $flag:False$ $list:[(p,_,_)]$ ; ] >> when is_string_patt p -> True
+| _ -> False
+]
+;
+
 value install () = 
 let ef = EF.mk () in 
 let ef = EF.{ (ef) with
@@ -75,8 +110,17 @@ let ef = EF.{ (ef) with
     fun arg ->
       Some (rewrite_str_item arg z)
 
+  | <:str_item:< [%%test_unit $exp:_$ ; ] >> as z ->
+    fun arg ->
+      Some (rewrite_str_item arg z)
+
   | <:str_item:< [%%test value $flag:False$ $list:_$ ; ] >>
    as z when is_named_bool_test z ->
+    fun arg ->
+      Some (rewrite_str_item arg z)
+
+  | <:str_item:< [%%test_unit value $flag:False$ $list:_$ ; ] >>
+   as z when is_named_unit_test z ->
     fun arg ->
       Some (rewrite_str_item arg z)
   ] } in
